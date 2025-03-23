4e33608369a026760dde873d8b814a3ce2f15e7e
Vineeth VS <138604414+vsVINEETH@users.noreply.github.com>
2025-03-24 00:39:30 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,13 @@ import 'firebase/firebase'
 import 'firebase/storage'
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAGe6LtOtE9QMQDmU-EbdWXAYT9rtZe1oI",
-    authDomain: "olx-clone-d8999.firebaseapp.com",
-    projectId: "olx-clone-d8999",
-    storageBucket: "olx-clone-d8999.appspot.com",
-    messagingSenderId: "482887308212",
-    appId: "1:482887308212:web:8a55386c0c99d6d77e58e1",
-    measurementId: "G-BNXYM53E1L"
+    apiKey: process.env.FIRE_BASE_KEY,
+    authDomain: process.env.FIRE_BASE_DOMAIN,
+    projectId: process.env.FIRE_BASE_PROJECT_ID ,
+    storageBucket: process.env.FIRE_BASE_BUCKET,
+    messagingSenderId: process.env.FIRE_BASE_SENDER_ID,
+    appId: process.env.FIRE_BASE_APP_ID,
+    measurementId: process.env.FIRE_BASE_MEASUREMENT_ID
   };
 
  export default firebase.initializeApp(firebaseConfig)
